a3f53b951f34f2b45ab9e3940f46e9dc0b97800d
Yohann Roussel <yroussel@google.com>
2014-04-07 12:23:40 +0200

    Remove duplicate hamcrest-core-jack target.
    Change-Id: I30dcf98cc1231021e94813f428ea55c8de45e7bc

--- hamcrest-core/Android.mk
+++ hamcrest-core/Android.mk
@@ -39,13 +39,3 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_HOST_JAVA_LIBRARY)
 
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_MODULE := hamcrest-core-jack
-
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_HOST_JAVA_LIBRARY)
