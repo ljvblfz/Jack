99db13d7a5f6673cacbd1e7d20bae1d5ad387c3e
Benoit Lamarche <benoitlamarche@google.com>
2014-04-14 11:51:40 +0200

    Add more tests to FileConflictTest
    And activate working tests
    
    Change-Id: Ide5bf712a4cd0dfb12830f62898f3ee0cfe22dbf

--- jack/tests/com/android/jack/ResourceTest.java
+++ jack/tests/com/android/jack/ResourceTest.java
@@ -141,7 +141,6 @@ public class ResourceTest {
   }
 
   @Test
-  @Category(KnownBugs.class)
   public void testJackDirToJackArchive() throws Exception {
     // compile source file to a Jack dir
     File jackFolder = TestTools.createTempDir("tempjack", "dir");
@@ -173,7 +172,6 @@ public class ResourceTest {
   }
 
   @Test
-  @Category(KnownBugs.class)
   public void testJackArchiveToJackDir() throws Exception {
     // compile source file to a Jack archive and add resources
     File jackAr = createJackArchiveWithResources();
@@ -196,7 +194,6 @@ public class ResourceTest {
   }
 
   @Test
-  @Category(KnownBugs.class)
   public void testJackDirToJackDir() throws Exception {
     /// compile source file to a Jack dir
     File jackFolder = TestTools.createTempDir("tempjack", "dir");
