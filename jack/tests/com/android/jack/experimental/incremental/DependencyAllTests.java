513fc66cc85f8c702f4f4fd18ac2c1f1126e8ca3
mikaelpeltier <mikaelpeltier@google.com>
2014-05-20 14:59:23 +0200

    Incremental support, add dependency on class literal usages
    Change-Id: I29dd96935b4e8c3cfcf04ea52f6933359c096b89

--- jack/tests/com/android/jack/experimental/incremental/DependencyAllTests.java
+++ jack/tests/com/android/jack/experimental/incremental/DependencyAllTests.java
@@ -8,6 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses(value = {DependenciesTest001.class, DependenciesTest002.class,
     DependenciesTest003.class, DependenciesTest004.class, DependenciesTest005.class,
     DependenciesTest006.class, DependenciesTest007.class, DependenciesTest008.class,
-    DependenciesTest009.class, DependenciesTest010.class, DependenciesTest011.class})
+    DependenciesTest009.class, DependenciesTest010.class, DependenciesTest011.class,
+    DependenciesTest012.class})
 public class DependencyAllTests {
 }
