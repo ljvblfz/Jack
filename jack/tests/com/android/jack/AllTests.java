ca1402550d9558baa7e63552962f5d357247da2e
Yohann Roussel <yroussel@google.com>
2014-09-09 11:47:06 +0200

    A test for phantom lookup conflicting with defined type.
    Change-Id: I264614217ba3b4351a86acac8db50351bfc9d1f8

--- jack/tests/com/android/jack/AllTests.java
+++ jack/tests/com/android/jack/AllTests.java
@@ -86,6 +86,7 @@ import org.junit.runners.Suite.SuiteClasses;
     InnerTest.class,
     InvokesTest.class,
     JarjarTest.class,
+    LookupTest.class,
     MainTest.class,
     MarkerCollectorTest.class,
     MergerAllTests.class,
