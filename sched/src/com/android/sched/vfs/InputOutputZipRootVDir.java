6c7bcc2c5f4b316ce9eccf67edb98dc6a9a6565f
Benoit Lamarche <benoitlamarche@google.com>
2014-10-21 17:46:26 +0200

    Add InputOutputZipVDir
    Change-Id: I0d48474882c25463c523edb80c22efe195faf1e0

---
+++ sched/src/com/android/sched/vfs/InputOutputZipRootVDir.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.sched.vfs;
+
+import com.google.common.io.Files;
+
+import com.android.sched.util.file.FileUtils;
+import com.android.sched.util.file.NotFileOrDirectoryException;
+import com.android.sched.util.file.OutputZipFile;
+import com.android.sched.util.location.FileLocation;
+import com.android.sched.util.stream.ByteStreamSucker;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.annotation.Nonnull;
+
+/**
+ * The root of a VFS backed by a real filesystem directory, compressed into a zip archive when
+ * closed.
+ */
+public class InputOutputZipRootVDir extends InputOutputZipVDir implements InputRootVDir,
+    InputOutputVDir, Closeable {
+
+  public InputOutputZipRootVDir(@Nonnull OutputZipFile zipFile) {
+    super(Files.createTempDir(), zipFile, new ZipEntry(""));
+  }
+
+  @Nonnull
+  @Override
+  public String getName() {
+    return "";
+  }
+
+  @Override
+  @Nonnull
+  public InputVFile getInputVFile(@Nonnull VPath path) throws NotFileOrDirectoryException {
+    File file = new File(dir, path.getPathAsString(File.separatorChar));
+    if (!file.isFile()) {
+      throw new NotFileOrDirectoryException(new FileLocation(file));
+    }
+    return new InputOutputZipVFile(file, zipFile, new ZipEntry(path.getPathAsString('/')));
+  }
+
+  @Override
+  public void close() throws IOException {
+    ZipOutputStream zos = zipFile.getOutputStream();
+    try {
+      addDirToZip(zos, this);
+    } finally {
+      zos.close();
+      FileUtils.deleteDir(dir);
+    }
+  }
+
+  private void addDirToZip(@Nonnull ZipOutputStream zos, @Nonnull InputOutputZipVDir vDir)
+      throws IOException {
+    for (InputVElement sub : vDir.list()) {
+      if (sub.isVDir()) {
+        addDirToZip(zos, (InputOutputZipVDir) sub);
+      } else {
+        InputOutputZipVFile vFile = (InputOutputZipVFile) sub;
+        zos.putNextEntry(vFile.getZipEntry());
+        InputStream is = vFile.openRead();
+        try {
+          new ByteStreamSucker(is, zos).suck();
+        } finally {
+          is.close();
+        }
+      }
+    }
+  }
+}
---
+++ sched/src/com/android/sched/vfs/InputOutputZipVDir.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.sched.vfs;
+
+import com.android.sched.util.ConcurrentIOException;
+import com.android.sched.util.file.CannotCreateFileException;
+import com.android.sched.util.file.OutputZipFile;
+import com.android.sched.util.location.DirectoryLocation;
+import com.android.sched.util.location.Location;
+import com.android.sched.util.location.ZipLocation;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.zip.ZipEntry;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+/**
+ * A directory in an {@link InputOutputZipRootVDir} VFS.
+ */
+class InputOutputZipVDir extends AbstractVElement implements InputOutputVDir {
+
+  @Nonnull
+  private static final char ZIP_ENTRY_SEPARATOR = '/';
+  @Nonnull
+  protected final File dir;
+  @CheckForNull
+  private ArrayList<InputVElement> list;
+  @Nonnull
+  private final Location location;
+  @Nonnull
+  protected final OutputZipFile zipFile;
+  @Nonnull
+  private final ZipEntry zipEntry;
+
+  public InputOutputZipVDir(@Nonnull File dir, @Nonnull OutputZipFile zipFile,
+      @Nonnull ZipEntry zipEntry) {
+    location = new ZipLocation(zipFile.getLocation(), zipEntry);
+    this.zipFile = zipFile;
+    this.dir = dir;
+    this.zipEntry = zipEntry;
+  }
+
+  @Nonnull
+  @Override
+  public String getName() {
+    return dir.getName();
+  }
+
+  @Nonnull
+  @Override
+  public synchronized Collection<? extends InputVElement> list() {
+    if (list == null) {
+      File[] subs = dir.listFiles();
+      if (subs == null) {
+        throw new ConcurrentIOException(new ListDirException(dir));
+      }
+      if (subs.length == 0) {
+        return Collections.emptyList();
+      }
+
+      ArrayList<InputVElement> localList = new ArrayList<InputVElement>(subs.length);
+      for (File sub : subs) {
+        String zipEntryName = zipEntry.getName();
+        String subZipEntryName;
+        if (zipEntryName.isEmpty()) {
+          subZipEntryName = sub.getName();
+        } else {
+          subZipEntryName = zipEntryName + ZIP_ENTRY_SEPARATOR + sub.getName();
+        }
+        ZipEntry subZipEntry = new ZipEntry(subZipEntryName);
+        if (sub.isFile()) {
+          localList.add(new InputOutputZipVFile(sub, zipFile, subZipEntry));
+        } else {
+          localList.add(new InputOutputZipVDir(sub, zipFile, subZipEntry));
+        }
+      }
+      list = localList;
+    }
+
+    return list;
+  }
+
+  @Override
+  @Nonnull
+  public Location getLocation() {
+    return location;
+  }
+
+  @Override
+  @Nonnull
+  public OutputVFile createOutputVFile(@Nonnull VPath path) throws CannotCreateFileException {
+    File file = new File(dir, path.getPathAsString(getSeparator()));
+    if (!file.getParentFile().mkdirs() && !file.getParentFile().isDirectory()) {
+      throw new CannotCreateFileException(new DirectoryLocation(file.getParentFile()));
+    }
+    return new InputOutputZipVFile(file, zipFile, new ZipEntry(
+        zipEntry.getName() + ZIP_ENTRY_SEPARATOR + path.getPathAsString(ZIP_ENTRY_SEPARATOR)));
+  }
+
+  @Override
+  public char getSeparator() {
+    return File.separatorChar;
+  }
+
+  @Override
+  public boolean isVDir() {
+    return true;
+  }
+}
