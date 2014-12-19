d121a81f0bf0c7c74c2d7559eb0ba4924305f6d7
Jean-Marie Henaff <jmhenaff@google.com>
2014-12-19 17:56:40 +0100

    Add possibility tu run classes with main() on host and device
    Runners currently run only JUnit test case.
    
    Change-Id: I181da83f1b5c48f24f697844b85837728304646b

--- jack-tests/src/com/android/jack/test/runner/RuntimeRunner.java
+++ jack-tests/src/com/android/jack/test/runner/RuntimeRunner.java
@@ -34,6 +34,9 @@ public abstract class RuntimeRunner {
     this.rtEnvironmentRootDir = rtEnvironmentRootDir;
   }
 
+  public abstract int run(@Nonnull String[] options, @Nonnull String mainClasses,
+      @Nonnull File... classpathFiles) throws RuntimeRunnerException;
+
   public abstract int runJUnit(@Nonnull String[] options, @Nonnull String jUnitRunnerName,
       @Nonnull String[] jUnitTestClasses, @Nonnull File... classpathFiles)
       throws RuntimeRunnerException;
