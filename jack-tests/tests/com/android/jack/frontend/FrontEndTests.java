55a3102e8f62e4315a4d653a6699316144e5728a
Yohann Roussel <yroussel@google.com>
2015-03-31 11:23:08 +0200

    Test duplicate classes compilation error
    Bug: 19989019
    Change-Id: I77e2d9d8a9465ed27f180bbc664f248c37dc9e33

--- jack-tests/tests/com/android/jack/frontend/FrontEndTests.java
+++ jack-tests/tests/com/android/jack/frontend/FrontEndTests.java
@@ -115,6 +115,37 @@ public class FrontEndTests {
   }
 
   /**
+   * Test that we do not crash and that we report the error.
+   */
+  @Test
+  @Category(KnownBugs.class)
+  public void testDuplicated001() throws Exception {
+    File outDir = AbstractTestTools.createTempDir();
+
+    JackApiToolchainBase toolchain =
+        AbstractTestTools.getCandidateToolchain(JackApiToolchainBase.class);
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    ByteArrayOutputStream err = new ByteArrayOutputStream();
+    toolchain.setOutputStream(out);
+    toolchain.setErrorStream(err);
+
+    try {
+      toolchain.addToClasspath(toolchain.getDefaultBootClasspath())
+      .srcToLib(
+          outDir,
+          /* zipFiles= */ false,
+          AbstractTestTools.getTestRootDir("com.android.jack.frontend.test016.jack"));
+      Assert.fail();
+    } catch (FrontendCompilationException e) {
+      Assert.assertEquals(0, out.size());
+      String errString = err.toString();
+      Assert.assertTrue(errString.contains("ERROR:"));
+      Assert.assertTrue(errString.contains("Duplicated"));
+   }
+  }
+
+
+  /**
    * Test that Jack is neither failing nor dropping warnings while ecj frontend is subject to skip
    * the local classes.
    */
