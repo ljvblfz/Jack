0635c13e2a4581dec9e3b3e78c4b6dcd8327a2d7
mikaelpeltier <mikaelpeltier@google.com>
2015-05-19 16:02:15 +0200

    NotSimplifier must take into account floating types
    - cmpl-x and cmpg-x do not return the same value for Nan operands.
    Consequently !(a>b) can not be replace by a<=b when using floating
    types otherwise the condition will not be evaluated correctly at runtime.
    
    Bug: 21212402
    Change-Id: Idfd37459b0013e1d0fba274bbc0f9acc103bb605

--- jack-tests/tests/com/android/jack/comparison/ComparisonTests.java
+++ jack-tests/tests/com/android/jack/comparison/ComparisonTests.java
@@ -23,7 +23,6 @@ import com.android.jack.test.runtime.RuntimeTestInfo;
 import com.android.jack.test.toolchain.AbstractTestTools;
 
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
