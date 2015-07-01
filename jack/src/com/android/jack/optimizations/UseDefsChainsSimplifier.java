65cdcc9b519123fd810f0bcfe9663b669056547c
mikaelpeltier <mikaelpeltier@google.com>
2015-07-01 18:01:37 +0200

    Modify implementation of UseDefsChainsSimplifier
    - The new implementation avoid stack overflow exception and
    speedup the optimization by avoid to traversing all control
    flow graphs to detect variable modification.
    
    Bug: 22194728
    Bug: 23001845
    
    Change-Id: Iba76f7626f208ef309058c91bbd4b8a477b52982

--- jack/src/com/android/jack/optimizations/UseDefsChainsSimplifier.java
+++ jack/src/com/android/jack/optimizations/UseDefsChainsSimplifier.java
@@ -19,10 +19,7 @@ package com.android.jack.optimizations;
 import com.android.jack.Options;
 import com.android.jack.analysis.DefinitionMarker;
 import com.android.jack.analysis.UseDefsMarker;
-import com.android.jack.analysis.UsedVariableMarker;
-import com.android.jack.analysis.dfa.reachingdefs.ReachingDefsMarker;
 import com.android.jack.cfg.BasicBlock;
-import com.android.jack.cfg.BasicBlockMarker;
 import com.android.jack.cfg.ControlFlowGraph;
 import com.android.jack.ir.ast.JIfStatement;
 import com.android.jack.ir.ast.JMethod;
@@ -34,136 +31,160 @@ import com.android.jack.ir.ast.JVisitor;
 import com.android.jack.transformations.request.Replace;
 import com.android.jack.transformations.request.TransformationRequest;
 import com.android.jack.transformations.threeaddresscode.ThreeAddressCodeForm;
+import com.android.jack.util.ControlFlowHelper;
+import com.android.jack.util.UseDefHelper;
 import com.android.jack.util.filter.Filter;
 import com.android.sched.item.Description;
 import com.android.sched.schedulable.Constraint;
 import com.android.sched.schedulable.RunnableSchedulable;
+import com.android.sched.schedulable.Use;
 import com.android.sched.util.config.ThreadConfig;
+import com.android.sched.util.log.Tracer;
+import com.android.sched.util.log.TracerFactory;
+import com.android.sched.util.log.stats.Counter;
+import com.android.sched.util.log.stats.CounterImpl;
+import com.android.sched.util.log.stats.StatisticId;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.Nonnull;
 
 /**
- * Simplify use definitions chains.
+ * Optimization will transform
+ * a = b // called s0
+ * ...
+ * m(a) // called s1
+ * will be transform to:
+ * a = b
+ * ...
+ * m(b)
+ *
+ * Optimization can be apply if the following conditions are respected:
+ *  - condition (0) a into s1 must used only one definition of a
+ *  - condition (1) a must have a value and b must be variable reference
+ *  - condition (2) if s0 and s1 are in the same block then the variable b must not be redefine
+ *  between statement s0 and s1
+ *  - condition (3) if s0 and s1 are not into the same block then all definitions of b used
+ *  by s0 must reach the block containing s1 and this block must not redefine b between the
+ *  beginning of the block and s1
+ *
  */
 @Description("Simplify use definitions chains.")
 @Constraint(need = {UseDefsMarker.class, ThreeAddressCodeForm.class, ControlFlowGraph.class})
+@Use(UseDefHelper.class)
 public class UseDefsChainsSimplifier extends DefUsesAndUseDefsChainsSimplifier
     implements RunnableSchedulable<JMethod> {
 
   @Nonnull
+  private static final StatisticId<Counter> SIMPLIFIED_USE_DEF_SYNTH = new StatisticId<Counter>(
+      "jack.optimization.usedef.synthetic", "Synthetic use def chain simplified",
+      CounterImpl.class, Counter.class);
+
+  @Nonnull
+  private static final StatisticId<Counter> SIMPLIFIED_USE_DEF = new StatisticId<Counter>(
+      "jack.optimization.usedef.java", "Use def chain simplified",
+      CounterImpl.class, Counter.class);
+
+  @Nonnull
   private final Filter<JMethod> filter = ThreadConfig.get(Options.METHOD_FILTER);
+
 
   private class Visitor extends JVisitor {
 
-    @Override
-    public boolean visit(@Nonnull JStatement stmt) {
-      UsedVariableMarker uvm = stmt.getMarker(UsedVariableMarker.class);
+    @Nonnull
+    private final Tracer tracer;
 
-      if (uvm != null) {
-        List<JVariableRef> varRefToDelete = new ArrayList<JVariableRef>();
-        List<JVariableRef> varRefToAdd = new ArrayList<JVariableRef>();
-
-        for (JVariableRef usedVarRef : uvm.getUsedVariables()) {
-          UseDefsMarker udm = usedVarRef.getMarker(UseDefsMarker.class);
-          assert udm != null;
-
-          List<DefinitionMarker> usedDefsMarker = udm.getDefs();
-
-          /*
-           * The transformation try to optimize statements that use variables that are
-           * definitions initialized with another variables.
-           *
-           * For instance the following code:
-           * a = b
-           * m(a)
-           * will be transform to:
-           * m(b)
-           *
-           * The transformation can be apply only if a statement used only one definition of a
-           * variable, for instance it could not be possible to optimize the code if m(a) use
-           * two definitions of a (for instance a=b and a=true).
-           */
-          if (usedDefsMarker.size() == 1) {
-            DefinitionMarker defMarker = usedDefsMarker.get(0);
-
-            if (defMarker.hasValue()
-                && defMarker.getDefinedVariable().isSynthetic()
-                && defMarker.getValue() instanceof JVariableRef) {
-              JVariableRef defValue = (JVariableRef) defMarker.getValue();
-              JVariable var = defValue.getTarget();
-              BasicBlockMarker bbm = stmt.getMarker(BasicBlockMarker.class);
-              assert bbm != null;
-
-              /* Optimization to be valid must check that:
-               * - Used variable (in the above sample, variable b) must not be redefine
-               * between his usage (a=b) and the statement that will be optimize
-               * - There is only one definition of the used variable (b in the above sample)
-               * reaching the basic block of the statement to optimize, otherwise b could contains
-               * an unexpected value
-               */
-              if (hasOnlyOneDefinition(var, bbm.getBasicBlock()) && !hasDefBetweenStatement(var,
-                  (JStatement) defMarker.getDefinition().getParent(), stmt)) {
-
-                JVariableRef newVarRef = getNewVarRef(defValue);
-
-                UseDefsMarker newUdm = new UseDefsMarker();
-                newVarRef.addMarker(newUdm);
-
-                // Fill the UseDefsMarker for the new variable reference.
-                UseDefsMarker udmToMove = defValue.getMarker(UseDefsMarker.class);
-                assert udmToMove != null;
-                newUdm.addUsedDefinitions(udmToMove.getDefs(), newVarRef);
-
-                TransformationRequest tr = new TransformationRequest(stmt);
-                tr.append(new Replace(usedVarRef, newVarRef));
-                tr.commit();
-
-                varRefToDelete.add(usedVarRef);
-                varRefToAdd.add(newVarRef);
-
-                defMarker.removeUse(usedVarRef);
-              }
-            }
-          }
-        }
-
-        for (JVariableRef varRef : varRefToDelete) {
-          uvm.getUsedVariables().remove(varRef);
-          assert varRef.getMarker(UseDefsMarker.class) != null;
-        }
-
-        for (JVariableRef varRef : varRefToAdd) {
-          uvm.getUsedVariables().add(varRef);
-          assert varRef.getMarker(UseDefsMarker.class) != null;
-        }
-      }
-
-      return super.visit(stmt);
+    private Visitor() {
+      tracer = TracerFactory.getTracer();
     }
 
-    /**
-     * Check that there is only one definition of {@code var} reaching {@code bb}.
-     * @param var Check that {@code var} is defined only one time.
-     * @param bb Check that {@code var} is defined only one time at {@code bb}.
-     * @return true if only one definition of {@code var} reaching {@code bb}, false otherwise.
-     */
-    private boolean hasOnlyOneDefinition(@Nonnull JVariable var, @Nonnull BasicBlock bb) {
-      ReachingDefsMarker rdm = bb.getMarker(ReachingDefsMarker.class);
-      assert rdm != null;
-      boolean alreadyDefined = false;
+    @Override
+    public boolean visit(@Nonnull JStatement s1) {
+      List<JVariableRef> varsUsedBys1 = UseDefHelper.getUsedVariables(s1);
 
-      for (DefinitionMarker dm : rdm.getReachingDefs()) {
-        if (dm.getDefinedVariable() == var) {
-          if (alreadyDefined) {
-            return false;
+      // Copy variable used by s1 to update the list during the visit
+      for (JVariableRef varRefOfa : varsUsedBys1.toArray(new JVariableRef[varsUsedBys1.size()])) {
+        List<DefinitionMarker> defsOfa = UseDefHelper.getUsedDefinitions(varRefOfa);
+
+        if (defsOfa.size() == 1) {
+          DefinitionMarker defOfa = defsOfa.get(0);
+
+          if (stmtCanBeOptimized(s1, defOfa)) {
+
+            if (defOfa.getDefinedVariable().isSynthetic()) {
+              tracer.getStatistic(SIMPLIFIED_USE_DEF_SYNTH).incValue();
+            } else {
+              tracer.getStatistic(SIMPLIFIED_USE_DEF).incValue();
+            }
+
+            JVariableRef varRefb = (JVariableRef) defOfa.getValue();
+            JVariableRef newVarRefb = getNewVarRef(varRefb);
+
+            UseDefsMarker udmOfNewVarRefb = new UseDefsMarker();
+            udmOfNewVarRefb.addUsedDefinitions(UseDefHelper.getUsedDefinitions(varRefb),
+                newVarRefb);
+            newVarRefb.addMarker(udmOfNewVarRefb);
+
+            varsUsedBys1.add(newVarRefb);
+            varsUsedBys1.remove(varRefOfa);
+
+            defOfa.removeUse(varRefOfa);
+
+            TransformationRequest tr = new TransformationRequest(s1);
+            tr.append(new Replace(varRefOfa, newVarRefb));
+            tr.commit();
           }
-          alreadyDefined = true;
         }
       }
-      return true;
+
+      return super.visit(s1);
+    }
+
+    private boolean stmtCanBeOptimized(@Nonnull JStatement s1, @Nonnull DefinitionMarker defOfa) {
+      // Condition (1)
+      if (!defOfa.hasValue() || !(defOfa.getValue() instanceof JVariableRef)) {
+        return false;
+      }
+
+      JVariableRef varRefb = (JVariableRef) defOfa.getValue();
+      BasicBlock bbOfs1 = ControlFlowHelper.getBasicBlock(s1);
+      JStatement s0 = defOfa.getStatement();
+      assert s0 != null;
+      BasicBlock bbOfs0 = ControlFlowHelper.getBasicBlock(s0);
+      JVariable b = varRefb.getTarget();
+
+      // Condition (2)
+      if (bbOfs0 == bbOfs1) {
+        if (!hasLocalDef(b, bbOfs0, s0, s1)) {
+          return true;
+        }
+      } else {
+        // Condition (3)
+        List<DefinitionMarker> defsOfbUseFroms0 = UseDefHelper.getUsedDefinitions(varRefb);
+        if (bbHasOnlyDefinitions(bbOfs1, b, defsOfbUseFroms0)
+            && !hasLocalDef(b, bbOfs1, null, s1)) {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    private boolean bbHasOnlyDefinitions(@Nonnull BasicBlock bb, @Nonnull JVariable var,
+        @Nonnull List<DefinitionMarker> defsToFound) {
+      int nbDef = 0;
+
+      for (DefinitionMarker def : UseDefHelper.getReachingDefs(bb)) {
+        if (def.getDefinedVariable() == var) {
+          if (defsToFound.contains(def)) {
+            nbDef++;
+          } else {
+            return false;
+          }
+        }
+      }
+
+      return defsToFound.size() == nbDef;
     }
 
     @Override
@@ -187,13 +208,13 @@ public class UseDefsChainsSimplifier extends DefUsesAndUseDefsChainsSimplifier
         || !filter.accept(this.getClass(), method)) {
       return;
     }
-
     ControlFlowGraph cfg = method.getMarker(ControlFlowGraph.class);
     assert cfg != null;
 
+    Visitor visitor = new Visitor();
+
     for (BasicBlock bb : cfg.getNodes()) {
       for (JStatement stmt : bb.getStatements()) {
-        Visitor visitor = new Visitor();
         visitor.accept(stmt);
       }
     }
