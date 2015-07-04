13f263ced0885d227faa0b51ccf421850f435026
Yohann Roussel <yroussel@google.com>
2015-07-04 13:04:34 +0200

    Restart server before any update
    And add a small sleep after each server restart
    
    Change-Id: I43dfa9adc7b0be56a90fc05a89b35c1c6a0bbcf7

--- jack/Android.mk
+++ jack/Android.mk
@@ -174,9 +174,12 @@ $(LOCAL_INSTALLED_MODULE): $(JACK_JAR_INTERMEDIATE) $(JACK_DEFAULT_LIB)
 	$(hide) unzip -qd $<.tmp $<
 	$(hide) unzip -qd $<.tmp/jack-default-lib $(JACK_DEFAULT_LIB)
 	$(hide) jar -cfm $@ $(PRIVATE_JAR_MANIFEST) -C $<.tmp .
+	$(hide) $(jack_admin_script) stop-server || (exit 0)
 	$(hide) $(jack_admin_script) install-server $(HOST_OUT_JAVA_LIBRARIES)/jack-launcher.jar  $(HOST_OUT_JAVA_LIBRARIES)/jack-server.jar || (exit 0)
 	$(hide) $(jack_admin_script) start-server || (exit 0)
+	$(hide) sleep 1
 	$(hide) $(jack_admin_script) force-update server $(HOST_OUT_JAVA_LIBRARIES)/jack-server.jar || (rm $@; exit 47)
+	$(hide) sleep 1
 	$(hide) $(jack_admin_script) force-update jack $@ || (rm $@; exit 47)
 
 # Include this library in the build server's output directory
