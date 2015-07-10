473dca817cc9ce20a59cc9bc257db6a54186a3fa
Yohann Roussel <yroussel@google.com>
2015-07-10 09:32:02 +0200

    Sleep after stoping the server
    This is necessary in case the running server is older than the fix of stop
    waiting for connections to be closed.
    
    Change-Id: Ieb1aa483b8f2ffd505f43757345264c4c010bef8

--- jack/Android.mk
+++ jack/Android.mk
@@ -174,6 +174,7 @@ $(LOCAL_INSTALLED_MODULE): $(JACK_JAR_INTERMEDIATE) $(JACK_DEFAULT_LIB)
 	$(hide) unzip -qd $<.tmp/jack-default-lib $(JACK_DEFAULT_LIB)
 	$(hide) jar -cfm $@ $(PRIVATE_JAR_MANIFEST) -C $<.tmp .
 	$(hide) $(jack_admin_script) stop-server || (exit 0)
+	$(hide) sleep 5
 	$(hide) $(jack_admin_script) install-server $(HOST_OUT_JAVA_LIBRARIES)/jack-launcher.jar  $(HOST_OUT_JAVA_LIBRARIES)/jack-server.jar || (exit 0)
 ifneq ($(dist_goal),)
 	mkdir -p "$(DIST_DIR)/logs/jack/"
