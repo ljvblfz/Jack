6db46da7ba6822d6290e3ded771deee3145f81ed
Yohann Roussel <yroussel@google.com>
2015-09-07 10:41:30 +0200

    Remove sleep after stoping the jack server
    This is no longer needed : ports have changed we may no more
    compete with a too old server.
    
    Change-Id: Id2925f765075ec0eaa95b7a665c65adb9b64e350

--- jack/Android.mk
+++ jack/Android.mk
@@ -175,7 +175,6 @@ $(LOCAL_INSTALLED_MODULE): $(JACK_JAR_INTERMEDIATE) $(JACK_DEFAULT_LIB)
 	$(hide) jar -cfm $@ $(PRIVATE_JAR_MANIFEST) -C $<.tmp .
 	$(hide) $(jack_admin_script) stop-server 2>&1 || (exit 0)
 	$(hide) $(jack_admin_script) kill-server 2>&1 || (exit 0)
-	$(hide) sleep 10
 ifneq ($(dist_goal),)
 	$(hide) $(jack_admin_script) uninstall-server 2>&1 || (exit 0)
 endif
