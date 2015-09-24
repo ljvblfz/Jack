0b735240fbfa678cf879b93a56e741755e106a00
Jean-Marie Henaff <jmhenaff@google.com>
2015-09-24 10:17:26 +0200

    Add ecj interface methods tests
    Change-Id: I6e339cb25c487e14a206ec5e9515500ffa25c20c

---
+++ jack-tests/tests/com/android/jack/java8/Java8AllTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.jack.java8;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * JUnit test for compilation of Java 8 features
+ */
+@RunWith(Suite.class)
+@SuiteClasses(value = {
+    /* LambdaTest.class, */
+    EcjInterfaceMethodsTest.class,
+    StaticMethodTest.class,
+    DefaultMethodTest.class
+    })
+public class Java8AllTest {
+}
