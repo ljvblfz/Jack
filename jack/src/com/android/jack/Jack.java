35e67bdaa929e91aad39a20e96bf5acf161283f6
Jean-Philippe Lesot <jplesot@google.com>
2015-10-02 12:14:03 +0200

    Allow keepattributes to keep parameter name.
    Change-Id: I10f4a0acba8c079cdef81354e7f87b1161b4586d

--- jack/src/com/android/jack/Jack.java
+++ jack/src/com/android/jack/Jack.java
@@ -500,7 +500,8 @@ public abstract class Jack {
             if (!options.flags.keepAttribute("LineNumberTable")) {
               request.addFeature(RemoveLineNumber.class);
             }
-            if (!options.flags.getKeepParameterNames()) {
+            if (!options.flags.getKeepParameterNames()
+                && !options.flags.keepAttribute("LocalVariableTable")) {
               request.addFeature(RemoveParameterName.class);
             }
           }
