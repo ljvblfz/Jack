7f37436349879e8a76ce93e927bfc3136f807c05
mikaelpeltier <mikaelpeltier@google.com>
2015-11-02 11:28:25 +0100

    Optimize conditional with constant condition
    Bug: 25191080
    
    (cherry picked from commit e8bbfd9668449c6c9fc2d31af89f152961721b75)
    
    Change-Id: I9bc7fc6a74f42b555d236431bf0fb1dd95adad54

--- jack/src/com/android/jack/transformations/ast/TypeLegalizer.java
+++ jack/src/com/android/jack/transformations/ast/TypeLegalizer.java
@@ -280,6 +280,7 @@ public class TypeLegalizer implements RunnableSchedulable<JMethod> {
           JExpression newDimension = dimension;
           if (!(dimension.getType() instanceof JPrimitiveType)) {
             newDimension = unbox(dimension);
+            tr.append(new Replace(dimension, newDimension));
             assert newDimension.getType() instanceof JIntegralType32;
           }
           castIfNeeded(newDimension, JPrimitiveTypeEnum.INT.getType());
@@ -326,6 +327,7 @@ public class TypeLegalizer implements RunnableSchedulable<JMethod> {
 
       if (!(expr.getType() instanceof JPrimitiveType)) {
         unboxedExpr = unbox(expr);
+        tr.append(new Replace(expr, unboxedExpr));
       }
 
       return unboxedExpr;
@@ -338,163 +340,13 @@ public class TypeLegalizer implements RunnableSchedulable<JMethod> {
       if (!(expectedType instanceof JPrimitiveType) && (type instanceof JPrimitiveType)) {
         assert expectedType instanceof JClassOrInterface;
         boxUnboxExpr = box(expr, (JClassOrInterface) expectedType);
+        tr.append(new Replace(expr, boxUnboxExpr));
       } else if ((expectedType instanceof JPrimitiveType) && !(type instanceof JPrimitiveType)) {
         boxUnboxExpr = unbox(expr);
+        tr.append(new Replace(expr, boxUnboxExpr));
       }
 
       return boxUnboxExpr;
-    }
-
-    @Nonnull
-    private JExpression box(@Nonnull JExpression exprToBox,
-        @Nonnull JClassOrInterface expectedType) {
-      assert exprToBox.getType() instanceof JPrimitiveType;
-
-      JMethodCall boxMethodCall = getBoxingCall(exprToBox, expectedType,
-          (JPrimitiveType) exprToBox.getType());
-
-      tr.append(new Replace(exprToBox, boxMethodCall));
-
-      return boxMethodCall;
-    }
-
-
-    @Nonnull
-    private JExpression unbox(@Nonnull JExpression exprToUnbox) {
-      JType typeToUnbox = exprToUnbox.getType();
-      assert !(typeToUnbox instanceof JPrimitiveType);
-      assert typeToUnbox instanceof JClassOrInterface;
-      JClassOrInterface wrapper = (JClassOrInterface) typeToUnbox;
-
-      String methodName;
-      JType returnType;
-
-      if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_BOOLEAN))) {
-        methodName = "booleanValue";
-        returnType = JPrimitiveTypeEnum.BOOLEAN.getType();
-      } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_BYTE))) {
-        methodName = "byteValue";
-        returnType = JPrimitiveTypeEnum.BYTE.getType();
-      } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_CHAR))) {
-        methodName = "charValue";
-        returnType = JPrimitiveTypeEnum.CHAR.getType();
-      } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_SHORT))) {
-        methodName = "shortValue";
-        returnType = JPrimitiveTypeEnum.SHORT.getType();
-      } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_INTEGER))) {
-        methodName = "intValue";
-        returnType = JPrimitiveTypeEnum.INT.getType();
-      } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_FLOAT))) {
-        methodName = "floatValue";
-        returnType = JPrimitiveTypeEnum.FLOAT.getType();
-      } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_DOUBLE))) {
-        methodName = "doubleValue";
-        returnType = JPrimitiveTypeEnum.DOUBLE.getType();
-      } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_LONG))) {
-        methodName = "longValue";
-        returnType = JPrimitiveTypeEnum.LONG.getType();
-      } else {
-        throw new AssertionError();
-      }
-
-
-      JMethodId unboxMethod = wrapper.getOrCreateMethodId(methodName, Lists.<JType>create(),
-      MethodKind.INSTANCE_VIRTUAL);
-      JMethodCall unboxMethodCall = new JMethodCall(
-          exprToUnbox.getSourceInfo(), exprToUnbox, wrapper,
-          unboxMethod, returnType, unboxMethod.canBeVirtual());
-
-      tr.append(new Replace(exprToUnbox, unboxMethodCall));
-
-      return unboxMethodCall;
-    }
-
-    // TODO(mikaelpeltier): Put it into JPrimitiveType
-    @Nonnull
-    private JMethodCall getBoxingCall(
-        @Nonnull JExpression exprToBox,
-        @Nonnull JClassOrInterface type,
-        @Nonnull JPrimitiveType pType) {
-      JClassOrInterface wrapperType = type;
-      JType argType;
-      JPhantomLookup lookup = Jack.getSession().getPhantomLookup();
-      if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_BOOLEAN))) {
-        argType = JPrimitiveTypeEnum.BOOLEAN.getType();
-      } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_BYTE))) {
-        argType = JPrimitiveTypeEnum.BYTE.getType();
-      } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_CHAR))) {
-        argType = JPrimitiveTypeEnum.CHAR.getType();
-      } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_SHORT))) {
-        argType = JPrimitiveTypeEnum.SHORT.getType();
-      } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_INTEGER))) {
-        argType = JPrimitiveTypeEnum.INT.getType();
-      } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_FLOAT))) {
-        argType = JPrimitiveTypeEnum.FLOAT.getType();
-      } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_DOUBLE))) {
-        argType = JPrimitiveTypeEnum.DOUBLE.getType();
-      } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_LONG))) {
-        argType = JPrimitiveTypeEnum.LONG.getType();
-      } else {
-        argType = pType;
-        switch (pType.getPrimitiveTypeEnum()) {
-          case BOOLEAN: {
-            wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_BOOLEAN);
-            break;
-          }
-          case BYTE: {
-            wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_BYTE);
-            break;
-          }
-          case CHAR: {
-            wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_CHAR);
-            break;
-          }
-          case SHORT: {
-            wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_SHORT);
-            break;
-          }
-          case INT: {
-            wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_INTEGER);
-            break;
-          }
-          case FLOAT: {
-            wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_FLOAT);
-            break;
-          }
-          case DOUBLE: {
-            wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_DOUBLE);
-            break;
-          }
-          case LONG: {
-            wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_LONG);
-            break;
-          }
-          default: {
-            throw new AssertionError();
-          }
-        }
-      }
-
-
-      JMethodId methodId = wrapperType.getOrCreateMethodId("valueOf", Lists.create(argType),
-          MethodKind.STATIC);
-      JMethodCall boxMethodCall = new JMethodCall(
-          exprToBox.getSourceInfo(), null, wrapperType, methodId, wrapperType,
-          methodId.canBeVirtual());
-      List<JType> paramTypes = methodId.getParamTypes();
-      assert paramTypes.size() == 1;
-      JType paramType = paramTypes.get(0);
-      JType exprToBoxType = exprToBox.getType();
-      JExpression arg;
-      if (exprToBoxType instanceof JNumericType && !paramType.isSameType(exprToBoxType)) {
-        assert paramType instanceof JNumericType;
-        arg = new JDynamicCastOperation(exprToBox.getSourceInfo(), exprToBox, paramType);
-      } else {
-        arg = exprToBox;
-      }
-      boxMethodCall.addArg(arg);
-
-      return boxMethodCall;
     }
 
     private void castIfNeeded(@Nonnull JExpression exprToCast, @Nonnull JType expectedType) {
@@ -518,4 +370,157 @@ public class TypeLegalizer implements RunnableSchedulable<JMethod> {
     tr.commit();
   }
 
+  @Nonnull
+  public static JExpression box(@Nonnull JExpression exprToBox,
+      @Nonnull JClassOrInterface expectedType) {
+    assert exprToBox.getType() instanceof JPrimitiveType;
+
+    JMethodCall boxMethodCall = getBoxingCall(exprToBox, expectedType,
+        (JPrimitiveType) exprToBox.getType());
+
+    return boxMethodCall;
+  }
+
+  @Nonnull
+  public static JExpression unbox(@Nonnull JExpression exprToUnbox,
+      @Nonnull JClassOrInterface typeToUnbox) {
+
+    String methodName;
+    JType returnType;
+
+    JPhantomLookup lookup = Jack.getSession().getPhantomLookup();
+
+    if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_BOOLEAN))) {
+      methodName = "booleanValue";
+      returnType = JPrimitiveTypeEnum.BOOLEAN.getType();
+    } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_BYTE))) {
+      methodName = "byteValue";
+      returnType = JPrimitiveTypeEnum.BYTE.getType();
+    } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_CHAR))) {
+      methodName = "charValue";
+      returnType = JPrimitiveTypeEnum.CHAR.getType();
+    } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_SHORT))) {
+      methodName = "shortValue";
+      returnType = JPrimitiveTypeEnum.SHORT.getType();
+    } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_INTEGER))) {
+      methodName = "intValue";
+      returnType = JPrimitiveTypeEnum.INT.getType();
+    } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_FLOAT))) {
+      methodName = "floatValue";
+      returnType = JPrimitiveTypeEnum.FLOAT.getType();
+    } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_DOUBLE))) {
+      methodName = "doubleValue";
+      returnType = JPrimitiveTypeEnum.DOUBLE.getType();
+    } else if (typeToUnbox.isSameType(lookup.getType(CommonTypes.JAVA_LANG_LONG))) {
+      methodName = "longValue";
+      returnType = JPrimitiveTypeEnum.LONG.getType();
+    } else {
+      throw new AssertionError();
+    }
+
+
+    JMethodId unboxMethod = typeToUnbox.getOrCreateMethodId(methodName, Lists.<JType>create(),
+    MethodKind.INSTANCE_VIRTUAL);
+    JMethodCall unboxMethodCall = new JMethodCall(
+        exprToUnbox.getSourceInfo(), exprToUnbox, typeToUnbox,
+        unboxMethod, returnType, unboxMethod.canBeVirtual());
+
+    return unboxMethodCall;
+  }
+
+  @Nonnull
+  private static JExpression unbox(@Nonnull JExpression exprToUnbox) {
+    JType typeToUnbox = exprToUnbox.getType();
+    assert !(typeToUnbox instanceof JPrimitiveType);
+    assert typeToUnbox instanceof JClassOrInterface;
+    return unbox(exprToUnbox, (JClassOrInterface) typeToUnbox);
+  }
+
+  // TODO(mikaelpeltier): Put it into JPrimitiveType
+  @Nonnull
+  private static JMethodCall getBoxingCall(
+      @Nonnull JExpression exprToBox,
+      @Nonnull JClassOrInterface type,
+      @Nonnull JPrimitiveType pType) {
+    JClassOrInterface wrapperType = type;
+    JType argType;
+    JPhantomLookup lookup = Jack.getSession().getPhantomLookup();
+    if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_BOOLEAN))) {
+      argType = JPrimitiveTypeEnum.BOOLEAN.getType();
+    } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_BYTE))) {
+      argType = JPrimitiveTypeEnum.BYTE.getType();
+    } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_CHAR))) {
+      argType = JPrimitiveTypeEnum.CHAR.getType();
+    } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_SHORT))) {
+      argType = JPrimitiveTypeEnum.SHORT.getType();
+    } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_INTEGER))) {
+      argType = JPrimitiveTypeEnum.INT.getType();
+    } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_FLOAT))) {
+      argType = JPrimitiveTypeEnum.FLOAT.getType();
+    } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_DOUBLE))) {
+      argType = JPrimitiveTypeEnum.DOUBLE.getType();
+    } else if (wrapperType.isSameType(lookup.getType(CommonTypes.JAVA_LANG_LONG))) {
+      argType = JPrimitiveTypeEnum.LONG.getType();
+    } else {
+      argType = pType;
+      switch (pType.getPrimitiveTypeEnum()) {
+        case BOOLEAN: {
+          wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_BOOLEAN);
+          break;
+        }
+        case BYTE: {
+          wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_BYTE);
+          break;
+        }
+        case CHAR: {
+          wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_CHAR);
+          break;
+        }
+        case SHORT: {
+          wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_SHORT);
+          break;
+        }
+        case INT: {
+          wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_INTEGER);
+          break;
+        }
+        case FLOAT: {
+          wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_FLOAT);
+          break;
+        }
+        case DOUBLE: {
+          wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_DOUBLE);
+          break;
+        }
+        case LONG: {
+          wrapperType = lookup.getClass(CommonTypes.JAVA_LANG_LONG);
+          break;
+        }
+        default: {
+          throw new AssertionError();
+        }
+      }
+    }
+
+
+    JMethodId methodId = wrapperType.getOrCreateMethodId("valueOf", Lists.create(argType),
+        MethodKind.STATIC);
+    JMethodCall boxMethodCall = new JMethodCall(
+        exprToBox.getSourceInfo(), null, wrapperType, methodId, wrapperType,
+        methodId.canBeVirtual());
+    List<JType> paramTypes = methodId.getParamTypes();
+    assert paramTypes.size() == 1;
+    JType paramType = paramTypes.get(0);
+    JType exprToBoxType = exprToBox.getType();
+    JExpression arg;
+    if (exprToBoxType instanceof JNumericType && !paramType.isSameType(exprToBoxType)) {
+      assert paramType instanceof JNumericType;
+      arg = new JDynamicCastOperation(exprToBox.getSourceInfo(), exprToBox, paramType);
+    } else {
+      arg = exprToBox;
+    }
+    boxMethodCall.addArg(arg);
+
+    return boxMethodCall;
+  }
 }
