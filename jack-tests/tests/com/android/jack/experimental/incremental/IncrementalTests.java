88c8547adfa24621d079975e53476eae36f306a1
Benoit Lamarche <benoitlamarche@google.com>
2016-04-15 17:25:32 +0200

    Add incremental workflow tests with resources
    Tests are currently failing and should be enabled when these workflows
    are fixed.
    
    Bug: 28240439
    
    Change-Id: Ieed3beb0289c3a83cc7444d63037d201a1bb4cb1

--- jack-tests/tests/com/android/jack/experimental/incremental/IncrementalTests.java
+++ jack-tests/tests/com/android/jack/experimental/incremental/IncrementalTests.java
@@ -17,17 +17,19 @@
 package com.android.jack.experimental.incremental;
 
 import com.android.jack.Options;
+import com.android.jack.library.InputJackLibrary;
+import com.android.jack.resource.ResourceTests;
+import com.android.jack.test.junit.KnownIssue;
 import com.android.jack.test.toolchain.AbstractTestTools;
 import com.android.jack.test.toolchain.IToolchain;
 import com.android.jack.test.toolchain.IncrementalToolchain;
 import com.android.jack.test.toolchain.JackBasedToolchain;
 import com.android.jack.test.toolchain.JillBasedToolchain;
-import com.android.jack.test.toolchain.LegacyJillToolchain;
-import com.android.jack.test.toolchain.TwoStepsToolchain;
 
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -45,10 +47,7 @@ public class IncrementalTests {
   public void testIncremental001() throws Exception {
 
     List<Class<? extends IToolchain>> excludeList = new ArrayList<Class<? extends IToolchain>>(1);
-    excludeList.add(LegacyJillToolchain.class);
     excludeList.add(IncrementalToolchain.class);
-    excludeList.add(TwoStepsToolchain.class);
-    excludeList.add(JillBasedToolchain.class);
 
     JackBasedToolchain toolchain =
         AbstractTestTools.getCandidateToolchain(JackBasedToolchain.class, excludeList);
@@ -76,4 +75,229 @@ public class IncrementalTests {
         AbstractTestTools.getTestRootDir("com.android.jack.experimental.incremental.test001.jack"));
   }
 
+  /**
+   * Incremental test with import of a resource dir, compiling sources to a dex directly.
+   */
+  @Test
+  @KnownIssue
+  public void testIncremental002ResourceImportToExe() throws Exception {
+
+    File srcDir =
+        AbstractTestTools.getTestRootDir("com.android.jack.experimental.incremental.test002.jack");
+    File incrementalDir = AbstractTestTools.createTempDir();
+
+    //copy resources to a tmp dir
+    File rscDestDir = AbstractTestTools.createTempDir();
+    File rscOriginalDir =
+        AbstractTestTools.getTestRootDir("com.android.jack.experimental.incremental.test002.rsc");
+    File rscOriginalFile1 = new File(rscOriginalDir, "resource1.txt");
+    AbstractTestTools.copyFileToDir(rscOriginalFile1, "resource1.txt", rscDestDir);
+    File rscDestFile1 = new File(rscDestDir, "resource1.txt");
+    File rscOriginalFile2 = new File(rscOriginalDir, "pack/resource2.txt");
+    AbstractTestTools.copyFileToDir(rscOriginalFile2, "pack/resource2.txt", rscDestDir);
+    File rscDestFile2 = new File(rscDestDir, "pack/resource2.txt");
+
+    JackBasedToolchain toolchain =
+        AbstractTestTools.getCandidateToolchain(JackBasedToolchain.class);
+    File dexDir = AbstractTestTools.createTempDir();
+    toolchain.addToClasspath(toolchain.getDefaultBootClasspath());
+    toolchain.setIncrementalFolder(incrementalDir);
+    toolchain.addResourceDir(rscDestDir);
+    toolchain.srcToExe(
+        dexDir,
+        /* zipFile = */ false,
+        srcDir);
+
+    ResourceTests.checkResourceContentFromDir(dexDir, "resource1.txt", "resource1");
+    ResourceTests.checkResourceContentFromDir(dexDir, "pack/resource2.txt", "resource2");
+
+    // modify resources and rebuild
+    FileWriter writer1 = new FileWriter(rscDestFile1);
+    try {
+      writer1.write("resource1.1");
+    } finally {
+      writer1.close();
+    }
+
+    FileWriter writer2 = new FileWriter(rscDestFile2);
+    try {
+      writer2.write("resource2.1");
+    } finally {
+      writer2.close();
+    }
+
+    toolchain =
+        AbstractTestTools.getCandidateToolchain(JackBasedToolchain.class);
+    File dexDir2 = AbstractTestTools.createTempDir();
+    toolchain.addToClasspath(toolchain.getDefaultBootClasspath());
+    toolchain.setIncrementalFolder(incrementalDir);
+    toolchain.addResourceDir(rscDestDir);
+    toolchain.srcToExe(
+        dexDir2,
+        /* zipFile = */ false,
+        srcDir);
+
+    ResourceTests.checkResourceContentFromDir(dexDir2, "resource1.txt", "resource1.1");
+    ResourceTests.checkResourceContentFromDir(dexDir2, "pack/resource2.txt", "resource2.1");
+  }
+
+  @Test
+  @KnownIssue
+  public void testIncremental002ResourceImportToLib() throws Exception {
+
+    List<Class<? extends IToolchain>> excludeList = new ArrayList<Class<? extends IToolchain>>(1);
+    excludeList.add(JillBasedToolchain.class); // We check Jack libraries, we don't want Jars
+
+    File srcDir =
+        AbstractTestTools.getTestRootDir("com.android.jack.experimental.incremental.test002.jack");
+    File incrementalDir = AbstractTestTools.createTempDir();
+
+    //copy resources to a tmp dir
+    File rscDestDir = AbstractTestTools.createTempDir();
+    File rscOriginalDir =
+        AbstractTestTools.getTestRootDir("com.android.jack.experimental.incremental.test002.rsc");
+    File rscOriginalFile1 = new File(rscOriginalDir, "resource1.txt");
+    AbstractTestTools.copyFileToDir(rscOriginalFile1, "resource1.txt", rscDestDir);
+    File rscDestFile1 = new File(rscDestDir, "resource1.txt");
+    File rscOriginalFile2 = new File(rscOriginalDir, "pack/resource2.txt");
+    AbstractTestTools.copyFileToDir(rscOriginalFile2, "pack/resource2.txt", rscDestDir);
+    File rscDestFile2 = new File(rscDestDir, "pack/resource2.txt");
+
+    JackBasedToolchain toolchain =
+        AbstractTestTools.getCandidateToolchain(JackBasedToolchain.class, excludeList);
+    File lib = AbstractTestTools.createTempFile("lib", toolchain.getLibraryExtension());
+    toolchain.addToClasspath(toolchain.getDefaultBootClasspath());
+    toolchain.setIncrementalFolder(incrementalDir);
+    toolchain.addResourceDir(rscDestDir);
+    toolchain.srcToLib(lib, /* zipFiles = */ true, srcDir);
+
+    // check lib
+    InputJackLibrary libObject = AbstractTestTools.getInputJackLibrary(lib);
+    try {
+      ResourceTests.checkResourceContentFromLib(libObject, "resource1.txt", "resource1");
+      ResourceTests.checkResourceContentFromLib(libObject, "pack/resource2.txt", "resource2");
+    } finally {
+      libObject.close();
+    }
+
+    // modify resources and rebuild
+    FileWriter writer1 = new FileWriter(rscDestFile1);
+    try {
+      writer1.write("resource1.1");
+    } finally {
+      writer1.close();
+    }
+
+    FileWriter writer2 = new FileWriter(rscDestFile2);
+    try {
+      writer2.write("resource2.1");
+    } finally {
+      writer2.close();
+    }
+
+    toolchain =
+        AbstractTestTools.getCandidateToolchain(JackBasedToolchain.class, excludeList);
+    toolchain.addToClasspath(toolchain.getDefaultBootClasspath());
+    toolchain.setIncrementalFolder(incrementalDir);
+    toolchain.addResourceDir(rscDestDir);
+    toolchain.srcToLib(lib, /* zipFiles = */ true, srcDir);
+
+    // check lib
+    libObject = AbstractTestTools.getInputJackLibrary(lib);
+    try {
+      ResourceTests.checkResourceContentFromLib(libObject, "resource1.txt", "resource1.1");
+      ResourceTests.checkResourceContentFromLib(libObject, "pack/resource2.txt", "resource2.1");
+    } finally {
+      libObject.close();
+    }
+  }
+
+  @Test
+  @KnownIssue
+  public void testIncremental002ResourceImportToExePlusLib() throws Exception {
+
+    List<Class<? extends IToolchain>> excludeList = new ArrayList<Class<? extends IToolchain>>(1);
+    excludeList.add(JillBasedToolchain.class); // We check Jack libraries, we don't want Jars
+
+    File srcDir =
+        AbstractTestTools.getTestRootDir("com.android.jack.experimental.incremental.test002.jack");
+    File incrementalDir = AbstractTestTools.createTempDir();
+
+    //copy resources to a tmp dir
+    File rscDestDir = AbstractTestTools.createTempDir();
+    File rscOriginalDir =
+        AbstractTestTools.getTestRootDir("com.android.jack.experimental.incremental.test002.rsc");
+    File rscOriginalFile1 = new File(rscOriginalDir, "resource1.txt");
+    AbstractTestTools.copyFileToDir(rscOriginalFile1, "resource1.txt", rscDestDir);
+    File rscDestFile1 = new File(rscDestDir, "resource1.txt");
+    File rscOriginalFile2 = new File(rscOriginalDir, "pack/resource2.txt");
+    AbstractTestTools.copyFileToDir(rscOriginalFile2, "pack/resource2.txt", rscDestDir);
+    File rscDestFile2 = new File(rscDestDir, "pack/resource2.txt");
+
+    JackBasedToolchain toolchain =
+        AbstractTestTools.getCandidateToolchain(JackBasedToolchain.class, excludeList);
+    File dexDir = AbstractTestTools.createTempDir();
+    File lib = AbstractTestTools.createTempFile("lib", toolchain.getLibraryExtension());
+    toolchain.addToClasspath(toolchain.getDefaultBootClasspath());
+    toolchain.setIncrementalFolder(incrementalDir);
+    toolchain.addResourceDir(rscDestDir);
+    toolchain.setOutputJack(lib, /* zipFiles = */ true);
+    toolchain.srcToExe(
+        dexDir,
+        /* zipFile = */ false,
+        srcDir);
+
+    // check dex dir
+    ResourceTests.checkResourceContentFromDir(dexDir, "resource1.txt", "resource1");
+    ResourceTests.checkResourceContentFromDir(dexDir, "pack/resource2.txt", "resource2");
+
+    // check lib
+    InputJackLibrary libObject = AbstractTestTools.getInputJackLibrary(lib);
+    try {
+      ResourceTests.checkResourceContentFromLib(libObject, "resource1.txt", "resource1");
+      ResourceTests.checkResourceContentFromLib(libObject, "pack/resource2.txt", "resource2");
+    } finally {
+      libObject.close();
+    }
+
+    // modify resources and rebuild
+    FileWriter writer1 = new FileWriter(rscDestFile1);
+    try {
+      writer1.write("resource1.1");
+    } finally {
+      writer1.close();
+    }
+
+    FileWriter writer2 = new FileWriter(rscDestFile2);
+    try {
+      writer2.write("resource2.1");
+    } finally {
+      writer2.close();
+    }
+
+    toolchain =
+        AbstractTestTools.getCandidateToolchain(JackBasedToolchain.class, excludeList);
+    File dexDir2 = AbstractTestTools.createTempDir();
+    toolchain.addToClasspath(toolchain.getDefaultBootClasspath());
+    toolchain.setIncrementalFolder(incrementalDir);
+    toolchain.addResourceDir(rscDestDir);
+    toolchain.setOutputJack(lib, /* zipFiles = */ true);
+    toolchain.srcToExe(
+        dexDir2,
+        /* zipFile = */ false,
+        srcDir);
+
+    // check dex dir
+    ResourceTests.checkResourceContentFromDir(dexDir2, "resource1.txt", "resource1.1");
+    ResourceTests.checkResourceContentFromDir(dexDir2, "pack/resource2.txt", "resource2.1");
+
+    // check lib
+    libObject = AbstractTestTools.getInputJackLibrary(lib);
+    try {
+      ResourceTests.checkResourceContentFromLib(libObject, "resource1.txt", "resource1.1");
+      ResourceTests.checkResourceContentFromLib(libObject, "pack/resource2.txt", "resource2.1");
+    } finally {
+        libObject.close();
+    }
+  }
 }
