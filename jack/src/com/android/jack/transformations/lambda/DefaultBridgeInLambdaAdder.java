bf039e38432bd89cc4c0ae21dc2dfeec7be431f9
Mikaël Peltier <mikaelpeltier@google.com>
2016-05-12 17:03:00 +0200

    Restrict usage of classpath to transform lambdas coming from Jill
    - Emit an error that the classpath is needed during lambda transformation
    for pre-N Android release only when a phantom interface and no bridge were
    found into the hierarchy of the interface to implement by a lambda.
    
    Bug: 28736446
    
    (cherry picked from commit 65608d16a886ac461c9f6a82421ba472d5063051)
    
    Change-Id: I9d7e58ea74122c7b3ee661c61ceb5f8e2f921ec4

--- jack/src/com/android/jack/transformations/lambda/DefaultBridgeInLambdaAdder.java
+++ jack/src/com/android/jack/transformations/lambda/DefaultBridgeInLambdaAdder.java
@@ -118,10 +118,14 @@ public class DefaultBridgeInLambdaAdder implements RunnableSchedulable<JMethod> 
 
     @Override
     public boolean visit(@Nonnull JLambda lambdaExpr) {
-      if (lambdaExpr.getMarker(LambdaFromJillMarker.class) != null) {
+      if (lambdaExpr.getMarker(LambdaFromJillMarker.class) != null
+          && lambdaExpr.getBridgeMethodIds().isEmpty()) {
+        // Lambdas come from Jill and does not have bridge information, search if default bridges
+        // exists to add it into lambda.
         List<JPhantomInterface> unknownInterfaces = new ArrayList<>();
-        addDefaultBridges(lambdaExpr, lambdaExpr.getType(), unknownInterfaces);
-        if (!unknownInterfaces.isEmpty()) {
+        boolean bridgesFound =
+            addDefaultBridges(lambdaExpr, lambdaExpr.getType(), unknownInterfaces);
+        if (!bridgesFound && !unknownInterfaces.isEmpty()) {
           session.getReporter().report(Severity.FATAL,
               new LambdaUnknownInterfaceReportable(lambdaExpr, unknownInterfaces));
           session.setAbortEventually(true);
@@ -130,8 +134,10 @@ public class DefaultBridgeInLambdaAdder implements RunnableSchedulable<JMethod> 
       return false;
     }
 
-    private void addDefaultBridges(@Nonnull JLambda lambdaExpr, @Nonnull JInterface interfaze,
+    private boolean addDefaultBridges(@Nonnull JLambda lambdaExpr, @Nonnull JInterface interfaze,
         @Nonnull List<JPhantomInterface> unknownInterfaces) {
+      boolean bridgeFound = false;
+
       if (interfaze instanceof JPhantomInterface) {
         unknownInterfaces.add((JPhantomInterface) interfaze);
       } else {
@@ -152,14 +158,22 @@ public class DefaultBridgeInLambdaAdder implements RunnableSchedulable<JMethod> 
               }
             }
             if (!bridgeAlreadyExists) {
+              bridgeFound = true;
               lambdaExpr.addBridgeMethodId(method.getMethodId());
             }
           }
         }
-        for (JInterface superInterface : definedInterface.getImplements()) {
-          addDefaultBridges(lambdaExpr, superInterface, unknownInterfaces);
+        if (!bridgeFound) {
+          for (JInterface superInterface : definedInterface.getImplements()) {
+            bridgeFound = addDefaultBridges(lambdaExpr, superInterface, unknownInterfaces);
+            if (bridgeFound) {
+              break;
+            }
+          }
         }
       }
+
+      return bridgeFound;
     }
   }
 
