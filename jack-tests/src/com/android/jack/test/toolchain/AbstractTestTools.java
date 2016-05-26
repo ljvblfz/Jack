ab56099bdcb6835daeaf98be8a160f0b18238dd7
Benoit Lamarche <benoitlamarche@google.com>
2016-05-26 10:43:24 +0200

    Remove unused import in AbstractTestTools
    Change-Id: Ia0c23005981e830300ce289d97317269c93671c6

--- jack-tests/src/com/android/jack/test/toolchain/AbstractTestTools.java
+++ jack-tests/src/com/android/jack/test/toolchain/AbstractTestTools.java
@@ -46,7 +46,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
