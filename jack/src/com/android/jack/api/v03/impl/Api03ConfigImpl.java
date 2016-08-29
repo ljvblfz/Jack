4b98f3fa2736da149a291ad794bbc202ed78b235
Jean-Philippe Lesot <jplesot@google.com>
2016-08-29 17:44:58 +0200

    Correct Api03Config for plugin path and names (cont.)
    Change-Id: I53096db65e8abaad9ab4d49ab163a3410b696908

--- jack/src/com/android/jack/api/v03/impl/Api03ConfigImpl.java
+++ jack/src/com/android/jack/api/v03/impl/Api03ConfigImpl.java
@@ -22,7 +22,7 @@ import com.android.jack.api.v02.impl.Api02ConfigImpl;
 import com.android.jack.api.v03.Api03Config;
 
 import java.io.File;
-import java.util.List;
+import java.util.Collection;
 
 import javax.annotation.Nonnull;
 
@@ -35,12 +35,12 @@ public class Api03ConfigImpl extends Api02ConfigImpl implements Api03Config {
   }
 
   @Override
-  public void setPluginPath(@Nonnull List<File> pluginPath) {
+  public void setPluginPath(@Nonnull Collection<File> pluginPath) {
     options.setPluginPath(Joiner.on(File.pathSeparator).join(pluginPath));
   }
 
   @Override
-  public void setPluginNames(@Nonnull List<String> pluginNames) {
+  public void setPluginNames(@Nonnull Collection<String> pluginNames) {
     options.setPluginNames(Joiner.on(',').join(pluginNames));
   }
 }
