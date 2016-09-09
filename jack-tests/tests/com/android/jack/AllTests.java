c90abad23bca63b74b6ad2a9a252d2ad3663e7d8
Jean-Philippe Lesot <jplesot@google.com>
2016-09-09 16:37:26 +0200

    Add encoding tests
    Change-Id: Icf3b2aefc2c408158c68861b1ff99b1f66148086

--- jack-tests/tests/com/android/jack/AllTests.java
+++ jack-tests/tests/com/android/jack/AllTests.java
@@ -19,6 +19,7 @@ import com.android.jack.constant.ConstantTests;
 import com.android.jack.debug.DebugTests;
 import com.android.jack.dextag.DexTagTests;
 import com.android.jack.dx.DxTests;
+import com.android.jack.encoding.tests.EncodingTests;
 import com.android.jack.enums.EnumsTests;
 import com.android.jack.error.ErrorHandlingAllTests;
 import com.android.jack.experimental.incremental.DependencyAllTests;
@@ -105,6 +106,7 @@ import org.junit.runners.Suite.SuiteClasses;
     DexTagTests.class,
     DuplicateSourceTest.class,
     DxTests.class,
+    EncodingTests.class,
     EnumsTests.class,
     ExternalTests.class,
     ErrorHandlingAllTests.class,
