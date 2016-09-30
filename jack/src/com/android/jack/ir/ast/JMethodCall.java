2705eca550b55d4316fa5a5bfdb026a522529a71
Mikaël Peltier <mikaelpeltier@google.com>
2016-09-30 11:12:24 +0200

    Add JAbstractMethodCall
    - Add a new JNode allowing to abstract method call with only
    one concrete class that is equals to the current JMethodCall.
    This will allow us to add a new JNode to represent polymorphic
    call to be able to manage it differently than the classical
    method call.
    
    Bug: 31543917
    Change-Id: I98e45beedeba58999bd1ac01a655523d0adeb691

--- jack/src/com/android/jack/ir/ast/JMethodCall.java
+++ jack/src/com/android/jack/ir/ast/JMethodCall.java
@@ -22,10 +22,6 @@ import com.android.sched.item.Description;
 import com.android.sched.scheduler.ScheduleInstance;
 import com.android.sched.transform.TransformRequest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 
@@ -33,7 +29,7 @@ import javax.annotation.Nonnull;
  * Java method call expression.
  */
 @Description("Java method call expression")
-public class JMethodCall extends JExpression {
+public class JMethodCall extends JAbstractMethodCall {
 
   /**
    * Dispatch kind of method call.
@@ -43,17 +39,6 @@ public class JMethodCall extends JExpression {
     DIRECT;
   }
 
-  @CheckForNull
-  private JExpression instance;
-  @Nonnull
-  private JClassOrInterface receiverType;
-  @Nonnull
-  private final ArrayList<JExpression> args = new ArrayList<JExpression>();
-
-  @Nonnull
-  private JMethodIdWide methodId;
-  @Nonnull
-  private final JType returnType;
   @Nonnull
   private final DispatchKind dispatchKind;
 
@@ -63,117 +48,18 @@ public class JMethodCall extends JExpression {
    * forces the caller to potentially deal with cloning objects if needed.
    */
   public JMethodCall(@Nonnull JMethodCall other, @CheckForNull JExpression instance) {
-    super(other.getSourceInfo());
-    this.instance = instance;
-    this.receiverType = other.receiverType;
-    this.methodId = other.methodId;
-    this.returnType = other.returnType;
-    this.dispatchKind = other.dispatchKind;
+    super(other.getSourceInfo(), instance, other.getReceiverType(), other.getMethodId(),
+        other.getType());
+    dispatchKind = other.getDispatchKind();
   }
 
   public JMethodCall(@Nonnull SourceInfo info, @CheckForNull JExpression instance,
       @Nonnull JClassOrInterface receiverType, @Nonnull JMethodIdWide methodId,
       @Nonnull JType returnType, boolean isVirtualDispatch) {
-    super(info);
-    assert (methodId != null);
+    super(info, instance, receiverType, methodId, returnType);
+    assert methodId != null;
     assert (!isVirtualDispatch) || methodId.getKind() == MethodKind.INSTANCE_VIRTUAL;
-    this.instance = instance;
-    this.receiverType = receiverType;
-    this.methodId = methodId;
-    this.returnType = returnType;
     this.dispatchKind = isVirtualDispatch ? DispatchKind.VIRTUAL : DispatchKind.DIRECT;
-  }
-
-  /**
-   * Adds an argument to this method.
-   */
-  public void addArg(@Nonnull JExpression toAdd) {
-    args.add(toAdd);
-  }
-
-  /**
-   * Adds arguments to this method.
-   */
-  public void addArgs(@Nonnull List<JExpression> toAdd) {
-    args.addAll(toAdd);
-  }
-
-  /**
-   * Adds an argument to this method.
-   */
-  public void addArgs(@Nonnull JExpression... toAdd) {
-    args.addAll(Arrays.asList(toAdd));
-  }
-
-  /**
-   * Returns the call arguments.
-   */
-  @Nonnull
-  public List<JExpression> getArgs() {
-    return args;
-  }
-
-  @CheckForNull
-  public JExpression getInstance() {
-    return instance;
-  }
-
-  @Nonnull
-  public JClassOrInterface getReceiverType() {
-    return receiverType;
-  }
-
-  protected void setReceiverType(@Nonnull JClassOrInterface receiverType) {
-    this.receiverType = receiverType;
-  }
-
-  @Override
-  public boolean canThrow() {
-    return true;
-  }
-
-  @Override
-  protected void transform(@Nonnull JNode existingNode, @CheckForNull JNode newNode,
-      @Nonnull Transformation transformation) throws UnsupportedOperationException {
-    if (!transform(args, existingNode, (JExpression) newNode, transformation)) {
-      super.transform(existingNode, newNode, transformation);
-    }
-  }
-
-  @Override
-  protected void replaceImpl(@Nonnull JNode existingNode, @Nonnull JNode newNode)
-      throws UnsupportedOperationException {
-    assert newNode != null;
-
-    if (instance == existingNode) {
-      instance = (JExpression) newNode;
-    } else {
-      super.replaceImpl(existingNode, newNode);
-    }
-  }
-
-  @Override
-  protected void removeImpl(@Nonnull JNode existingNode) throws UnsupportedOperationException {
-    if (instance == existingNode) {
-      instance = null;
-    } else {
-      super.removeImpl(existingNode);
-    }
-  }
-
-  @Nonnull
-  public JMethodIdWide getMethodId() {
-    return methodId;
-  }
-
-  @Nonnull
-  @Override
-  public JType getType() {
-    return returnType;
-  }
-
-  public void resolveMethodId(@Nonnull JMethodIdWide methodId) {
-    this.methodId = methodId;
   }
 
   @Override
@@ -190,32 +76,10 @@ public class JMethodCall extends JExpression {
     visitChildren(schedule);
   }
 
-  protected void visitChildren(JVisitor visitor) {
-    if (instance != null) {
-      visitor.accept(instance);
-    }
-    visitor.accept(args);
-  }
-
-  protected void visitChildren(@Nonnull ScheduleInstance<? super Component> schedule)
-      throws Exception {
-    if (instance != null) {
-      instance.traverse(schedule);
-    }
-    for (JExpression arg : args) {
-      arg.traverse(schedule);
-    }
-  }
-
   @Override
   public void visit(@Nonnull JVisitor visitor, @Nonnull TransformRequest transformRequest)
       throws Exception {
     visitor.visit(this, transformRequest);
-  }
-
-  @Nonnull
-  public String getMethodName() {
-    return methodId.getName();
   }
 
   @Nonnull
